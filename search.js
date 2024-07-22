33ec18e64d1f1a2fe952e78498c6de123fd096d5
Arunraj003 <arunr6506@gmail.com>
2024-07-22 19:53:55 +0530

    commit "sample"

--- search.js
+++ search.js
@@ -2,9 +2,14 @@ function filterCards() {
     const searchInput = document.getElementById('search-input').value.toLowerCase();
     const cards = document.getElementsByClassName('card');
 
+    console.log(`Search input: ${searchInput}`);
+    console.log(`Number of cards: ${cards.length}`);
+
     for (let i = 0; i < cards.length; i++) {
         const card = cards[i];
         const cardText = card.innerText.toLowerCase();
+
+        console.log(`Card ${i} text: ${cardText}`);
 
         if (cardText.includes(searchInput)) {
             card.style.display = 'block';
@@ -13,4 +18,3 @@ function filterCards() {
         }
     }
 }
-
