69561102adea7f5587e270fbb24e6326c2020213
Arunraj003 <arunr6506@gmail.com>
2024-07-31 18:13:08 +0530

    commit "alert added"

---
+++ js/alertHome.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Show an alert message
+    alert("Please register first to access all features!");
+
+    // Redirect to register.html
+    window.location.href = "register.html";
+});
